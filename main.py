854911fff8098197a025ca3611d6e104e6b69d1d
Ayumi <96675047+hitoesan@users.noreply.github.com>
2022-02-25 20:14:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#GABRIELA AYUMI ENDO GONDO - 04/2021
+#INTERNET DAS COISAS
+import dht
+import machine
+import time
+from wifi_lib import conecta
+import urequests
+
+d = dht.DHT11(machine.Pin(4))
+r = machine.Pin(2, machine.Pin.OUT)
+
+#Conecta à internet
+print("Conectando...")
+station = conecta("NET_2G3A151A", "")
+if not station.isconnected():
+    print("Não conectado!")
+else:
+    print("Conectado!")
+
+
+while True:
+        d.measure()            #Mede a temperatura e umidade
+        temp = d.temperature() #Armazena a temperatura
+        umid = d.humidity()    #Armazena a umidade
+        
+        if temp>31 or umid>70: #Condições para acender o relé
+            r.value(1)
+        else:
+            r.value(0)
+        
+        #Envio de dados para o Thingspeak via internet
+        resposta = urequests.get("https://api.thingspeak.com/update?api_key=0JGY1Z4W00USO8R9&field1={}&field2={}".format(temp, umid))
+        resposta.text
+    
+        #Imprime a temperatura e umidade
+        print("Temp={} Umid={}".format(temp, umid))
+        
+        #Repete o processo a cada 5s
+        time.sleep(5)
---
+++ wifi_lib.py
@@ -0,0 +1,15 @@
+#GABRIELA AYUMI ENDO GONDO - 04/2021
+#INTERNET DAS COISAS
+def conecta(ssid, senha):
+    import network
+    import time
+    
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(ssid, senha)
+    
+    for t in range(50):
+        if station.isconnected():
+            break
+        time.sleep(0.1)
+    return station
